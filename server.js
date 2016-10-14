d79876eb54fa034bbddd215062dfbb3167ce18e1
swetha <swethak266@gmail.com>
2016-10-14 06:22:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
- res.send("Article one will be here in few minutes");   
-});
+
 
 
 
