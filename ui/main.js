b06ef9ff69b66e1f51d166e2884bac274aa2446c
swetha <swethak266@gmail.com>
2016-10-14 09:07:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
  var text=document.getElementById('t');
  text.innerHTML ="new value";
  
- var img=document.getElementById('madi');
+ var img = document.getElementById('madi');
  img.onclick = function() {
      img.style.marginleft = '100px';
  };
