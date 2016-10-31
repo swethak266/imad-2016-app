204f8942d3b83d6efd1fb3f66d560a3a9687da09
swetha <swethak266@gmail.com>
2016-10-31 15:39:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/inspirational', function (req, res) {
+app.get('/inspirational', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui','inspirational.html'));
 });
 
