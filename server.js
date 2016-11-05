f816a96502424c0b2488fc01879360d8605e4b99
swetha <swethak266@gmail.com>
2016-11-05 17:28:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool= require('pg').pool;
+var crypto=require('crypto');
+
+var config={
+    user: 'swethak266',
+    database: 'swethak266',
+    host: 'db.imad.hasura-app.io',
+    port: '5678',
+    password: process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
