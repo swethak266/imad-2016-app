fdd5ea891ff7777c27fe177132cb5c29ddd3d292
swetha <swethak266@gmail.com>
2016-11-05 17:38:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,6 +25,23 @@ app.get('/hash/:input', function(req, res) {
    res.send(hashedString);
 });
 
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
+
 
 
 var app = express();
