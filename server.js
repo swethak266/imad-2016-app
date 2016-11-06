c327e8ae329e2906bafee4c2c5f7d7792012003e
swetha <swethak266@gmail.com>
2016-11-06 18:08:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -64,13 +64,12 @@ function createTemplate (data) {
         var currentArticleTitle = window.location.pathname.split('/')[2];
 
         function loadCommentForm () {
-            var commentFormHtml = `
-                <h5>Submit a comment</h5>
-                <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-                <br/>
-                <input type="submit" id="submit" value="Submit" />
-                <br/>
-                `;
+            var commentFormHtml = '<h5>Submit a comment</h5>';
+            commentFormHtml += '<textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>';
+            commentFormHtml += '<br/>';
+            commentFormHtml += '<input type="submit" id="submit" value="Submit" />';
+            commentFormHtml += '<br/>';
+              
             document.getElementById('comment_form').innerHTML = commentFormHtml;
 
             // Submit username/password to login
