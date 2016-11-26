e78aa46860445d01c0adf4e29a98eda8194322e9
swetha <swethak266@gmail.com>
2016-11-26 15:45:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,45 @@ function createTemplate (data) {
           <script type="text/javascript">
         var currentArticleTitle = window.location.pathname.split('/')[2];
 
-        .
+        function loadCommentForm () {
+            var commentFormHtml = '<h5>Submit a comment</h5>';
+            commentFormHtml += '<textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>';
+            commentFormHtml += '<br/>';
+            commentFormHtml += '<input type="submit" id="submit" value="Submit" />';
+            commentFormHtml += '<br/>';
+              
+            document.getElementById('comment_form').innerHTML = commentFormHtml;
+
+            // Submit username/password to login
+            var submit = document.getElementById('submit');
+            submit.onclick = function () {
+                // Create a request object
+                var request = new XMLHttpRequest();
+
+                // Capture the response and store it in a variable
+                request.onreadystatechange = function () {
+                  if (request.readyState === XMLHttpRequest.DONE) {
+                        // Take some action
+                        if (request.status === 200) {
+                            // clear the form & reload all the comments
+                            document.getElementById('comment_text').value = '';
+                            loadComments();    
+                        } else {
+                            alert('Error! Could not submit comment');
+                        }
+                        submit.value = 'Submit';
+                  }
+                };
+
+                // Make the request
+                var comment = document.getElementById('comment_text').value;
+                request.open('POST', '/submit-comment/' + currentArticleTitle, true);
+                request.setRequestHeader('Content-Type', 'application/json');
+                request.send(JSON.stringify({comment: comment}));  
+                submit.value = 'Submitting...';
+
+            };
+        }
 
         function loadLogin () {
             // Check if the user is already logged in
@@ -118,7 +156,7 @@ function createTemplate (data) {
         }
 
 
-        // The first thing to do is to check if the user is logged in!.
+        // The first thing to do is to check if the user is logged in!
         loadLogin();
         loadComments();
 
