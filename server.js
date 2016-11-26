7ff7cbd756ebb2425a74acf28fe55ab6884b0eba
swetha <swethak266@gmail.com>
2016-11-26 14:54:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -304,7 +304,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
                     var articleId = result.rows[0].id;
                     // Now insert the right comment for this article
                     pool.query(
-                        "INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)",
+                        'INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)',
                         [req.body.comment, articleId, req.session.auth.userId],
                         function (err, result) {
                             if (err) {
